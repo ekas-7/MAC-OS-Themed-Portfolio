a5d5bae45e74f8495c3e47d0f083bbcc10b5c76d
ekas-7 <ekaspreetatwal@gmail.com>
2025-01-04 17:07:00 +0530

    responsive design added

--- app/components/Window.tsx
+++ app/components/Window.tsx
@@ -17,9 +17,19 @@ export default function Window({  title, children, onClose }: WindowProps) {
   const [isMinimized, setIsMinimized] = useState(false);
   const [isResizing, setIsResizing] = useState(false);
   const [resizeOffset, setResizeOffset] = useState({ x: 0, y: 0 });
-
+  const isSmallDevice = typeof window !== "undefined" && window.innerWidth <= 768;
+  const defaultSize = isSmallDevice
+    ? { width: window.innerWidth * 0.9, height: window.innerHeight * 0.5 }
+    : { width: 800, height: 600 };
+  const defaultPosition = isSmallDevice
+    ? { x: window.innerWidth * 0.05, y: window.innerHeight * 0.1 }
+    : { x: 50, y: 50 };
   const windowRef = useRef<HTMLDivElement>(null);
 
+  useEffect (()=>{
+    setSize(defaultSize);
+    setPosition(defaultPosition);
+  },[])
   useEffect(() => {
     const timeout = setTimeout(() => {
       if (isMinimized) {
@@ -76,10 +86,10 @@ export default function Window({  title, children, onClose }: WindowProps) {
 
   const handleMaximize = () => {
     if (windowRef.current) {
-      setPosition({ x: 5, y: 35 });
+      setPosition({ x: 5, y: 40 });
       setSize({
         width: window.innerWidth - 10,
-        height: window.innerHeight - 40,
+        height: window.innerHeight - 50,
       });
       setIsMaximized(true);
       setIsMinimized(false);
@@ -88,8 +98,8 @@ export default function Window({  title, children, onClose }: WindowProps) {
 
   const handleMinimize = () => {
     if (windowRef.current) {
-      setPosition({ x: 50, y: 50 });
-      setSize({ width: 800, height: 600 });
+      setPosition(defaultPosition);
+      setSize(defaultSize);
       setIsMaximized(false);
       setIsMinimized(true);
     }
@@ -120,36 +130,33 @@ export default function Window({  title, children, onClose }: WindowProps) {
       }}
     >
       <div
-        className="bg-gray-100/80 dark:bg-gray-800/80 h-8 flex items-center justify-between px-3 cursor-move backdrop-blur-sm border-b border-white/10 dark:border-white/5 transition-colors duration-300"
-        onMouseDown={handleMouseDown}
-      >
-        <div className="flex items-center space-x-2">
-          <button
-            className="w-3 h-3 rounded-full bg-red-500 flex items-center justify-center"
-            onClick={onClose}
-          >
-            <X className="w-4 h-4 p-0.5 text-white dark:text-black" />
-          </button>
+  className="bg-gray-100/80 dark:bg-gray-800/80 sm:h-8 h-10 flex items-center justify-between px-3 cursor-move backdrop-blur-sm border-b border-white/10 dark:border-white/5 transition-colors duration-300"
+  onMouseDown={handleMouseDown}
+>
+  <div className="flex items-center space-x-2">
+    <button
+      className="w-5 h-5 sm:w-4 sm:h-4 lg:w-3 lg:h-3 rounded-full bg-red-500 flex items-center justify-center"
+      onClick={onClose}
+    >
+      <X className="w-6 h-6 sm:p-0.5 p-1 sm:w-5 sm:h-5 text-white dark:text-black" />
+    </button>
 
-          <button
-            className="w-3 h-3 rounded-full bg-yellow-500 flex items-center justify-center"
-            onClick={handleMinimize}
-          >
-            <Minus className="w-4 h-4 p-0.5 text-white dark:text-black" />
-          </button>
+    <button
+      className="w-5 h-5 sm:w-4 sm:h-4 lg:w-3 lg:h-3 rounded-full bg-yellow-500 flex items-center justify-center"
+      onClick={handleMinimize}
+    >
+      <Minus className="w-6 h-6 sm:p-0.5 p-1 sm:w-5 sm:h-5 text-white dark:text-black" />
+    </button>
 
-          <button
-            className="w-3 h-3 rounded-full bg-green-500 flex items-center justify-center"
-            onClick={handleMaximize}
-          >
-            <Square className="w-4 h-4 p-0.5 text-white dark:text-black" />
-          </button>
-        </div>
-        <span className="text-sm font-semibold text-gray-800 dark:text-gray-200">
-          {title}
-        </span>
-        <div className="w-16" />
-      </div>
+    <button
+      className="w-5 h-5 sm:w-4 sm:h-4 lg:w-3 lg:h-3 rounded-full bg-green-500 flex items-center justify-center"
+      onClick={handleMaximize}
+    >
+      <Square className="w-6 h-6 sm:p-0.5 p-1 sm:w-5 sm:h-5 text-white dark:text-black" />
+    </button>
+  </div>
+</div>
+
       <div className="bg-white/50 dark:bg-black/50 backdrop-blur-md  h-[calc(100%-2rem)] overflow-auto text-gray-800 dark:text-gray-200 transition-colors duration-300">
         {children}
       </div>
